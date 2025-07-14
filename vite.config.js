1ea47b26db34bb514e350d832cbd573fdece10ff
Mmesoma <mmesomaonwusika@gmail.com>
2025-07-14 16:21:46 +0100

    Initaial commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/svelte-about-me',    // right here! your repository name
   plugins: [svelte()],
 })
